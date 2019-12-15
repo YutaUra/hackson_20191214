7f0e763c4c349d46a734143bebcc113397a754fb
yuuta3594 <yuuta3594@outlook.jp>
2019-12-15 12:56:31 +0900

    fix

--- user/forms/__init__.py
+++ user/forms/__init__.py
@@ -15,19 +15,17 @@ class UserForm(UserCreationForm):
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
 
-
     class Meta:
         model = User
-        fields = ('name','birthday', 'gender', 'email')
+        fields = ('name', 'birthday', 'gender', 'email')
         MONTHS = {
             1: 'jan', 2: 'feb', 3: 'mar', 4: 'apr',
             5: 'may', 6: 'jun', 7: 'jul', 8: 'aug',
             9: 'sep', 10: 'oct', 11: 'nov', 12: 'dec'
         }
         widgets = {
-            'birthday': SelectDateWidget(months = MONTHS,years=[x for x in range(1900, 2030)])
+            'birthday': SelectDateWidget(months=MONTHS, years=[x for x in range(19, 2030)])
         }
-
 
 
 class LoginForm(AuthenticationForm):
@@ -37,4 +35,4 @@ class LoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['placeholder'] = field.label  #placeholderにフィールドのラベルを入れる
+            field.widget.attrs['placeholder'] = field.label  # placeholderにフィールドのラベルを入れる
